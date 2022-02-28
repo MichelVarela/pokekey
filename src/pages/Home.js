35dcc460cc5269c71c55b0977a7885beb18be13b
MichelVarela <michelvarela.95@outlook.com>
2022-02-28 00:39:19 -0300

    28.2 solution

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,6 @@ import unknow from '../images/unknow.png';
 
 const Home = () => {
 
-  const [dataURL, setDataURL] = useState([]);
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
@@ -20,41 +19,28 @@ const Home = () => {
         const min = Math.floor(Math.random() * 806 + 1);
         const res = await axios({url: `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${min}`})
         const {data} = res;
-        setDataURL(data.results)
+        
+        const dataPokemon = await axios.all(data.results.map(({url}) => axios.get(url)));
+        setPokemon(dataPokemon);
         
       } catch (err) {
         console.log(err);
       }
     }
 
-    const getPokemon = async () => { 
+    return getURL()
 
-      try {
-        getURL() 
-        const res = await axios.all(dataURL.map(({url}) => axios.get(url)))
-        setPokemon(res);
-        
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    return getPokemon(); 
-
-  }, [])   
-  
-  console.log(dataURL); 
-
-  //console.log(dataURL);
-  //console.log(pokemon)   
+  }, []);    
 
   return (
     <main className='home'>
         <h2>welcome to pokekey</h2>
 
-        {pokemon.map(({data}) => (
-            <CartPokemon key={data.name} order={data.order} name={data.name} sprites={data.sprites.other['official-artwork'].front_default ? data.sprites.other['official-artwork'].front_default : unknow} type={data.types[0].type.name}/> 
-        ))}
+        <div className="content-pokemon">
+          {pokemon.map(({data}) => (
+              <CartPokemon key={data.name} order={data.order} name={data.name} sprites={data.sprites.other['official-artwork'].front_default ? data.sprites.other['official-artwork'].front_default : unknow} type={data.types[0].type.name}/> 
+          ))}
+        </div>
     </main>
     )
 }; 
